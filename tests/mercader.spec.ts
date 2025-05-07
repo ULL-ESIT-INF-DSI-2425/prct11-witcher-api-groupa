a970df7276d73505fdbfd7099077ad05eac8d00f
DGarGlez <alu0101394749@ull.edu.es>
2025-05-07 18:32:53 +0000

    Primeros test hechos

--- tests/mercader.spec.ts
+++ tests/mercader.spec.ts
@@ -1,6 +1,12 @@
-import { describe, test } from "vitest";
+import { describe, test, beforeEach } from "vitest";
 import request from "supertest";
 import { app } from "../src/posada.js";
+import { Mercader } from "../src/models/mercader.js";
+
+// Borrar la base de datos antes de cada prueba
+beforeEach(async () => {
+  await Mercader.deleteMany();
+});
 
 describe("POST /mercaderes", () => {
   test("Debería crea un nuevo usuario correctamente.", async () => {
@@ -13,5 +19,21 @@ describe("POST /mercaderes", () => {
         ubicacion: "Novigrado"
       })
       .expect(201);
+  });
+});
+
+describe("GET /mercaderes", () => {
+  test("No debería obtener, puesto que no hay mercaderes.", async () => {
+    await request(app)
+      .get("/mercaderes")
+      .expect(404);
+  });
+});
+
+describe("GET /mercaderes", () => {
+  test("No debería obtener, puesto que no hay mercaderes.", async () => {
+    await request(app)
+      .get("/mercaderes?nombre=Eduardo Segredo")
+      .expect(404);
   });
 });
