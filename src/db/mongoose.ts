5842a713baa370df1f7ccd25d0cd74dcc8ca129b
DGarGlez <alu0101394749@ull.edu.es>
2025-05-08 18:26:44 +0000

    Final 3

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,31 +1,23 @@
-// import { connect } from 'mongoose';
-
-
-// const uri = process.env.MONGODB_URI;
-
-
-// if (!uri) {
-//   console.error("❌ La variable de entorno MONGODB_URI no está definida");
-//   process.exit(1); // Termina el proceso si no está configurada
-// }
-
-// connect(uri)
-//   .then(() => {
-//     console.log("✅ Conexión a MongoDB establecida");
-//   })
-//   .catch((error) => {
-//     console.error("❌ No se pudo conectar a MongoDB:", error);
-//     process.exit(1); // Termina el proceso en caso de error
-//   });
-
 import { connect } from 'mongoose';
 
-try {
-  await connect('mongodb://127.0.0.1:27017/posada-api');
-  console.log('Connection to MongoDB server established');
-} catch (error) {
-  console.log(error);
+
+const uri = process.env.MONGODB_URI;
+
+
+if (!uri) {
+  console.error("❌ La variable de entorno MONGODB_URI no está definida");
+  process.exit(1); // Termina el proceso si no está configurada
 }
+
+connect(uri)
+  .then(() => {
+    console.log("✅ Conexión a MongoDB establecida");
+  })
+  .catch((error) => {
+    console.error("❌ No se pudo conectar a MongoDB:", error);
+    process.exit(1); // Termina el proceso en caso de error
+  });
+
 
 // import { connect } from 'mongoose';
 
