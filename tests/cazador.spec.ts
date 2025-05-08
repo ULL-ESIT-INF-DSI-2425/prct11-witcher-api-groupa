f583a93781890bddbbad86cf27a0b0509db77bd3
DGarGlez <alu0101394749@ull.edu.es>
2025-05-08 16:43:20 +0000

    Integración coveralls

--- tests/cazador.spec.ts
+++ tests/cazador.spec.ts
@@ -1,6 +1,6 @@
 import { describe, test, beforeEach } from "vitest";
 import request from "supertest";
-import { app } from "../src/posada.js";
+import { app } from "../src/app.js";
 import { Cazador } from "../src/models/cazador.js";
 
 // Borrar la base de datos antes de cada prueba
@@ -30,7 +30,7 @@ describe("POST /cazadores, 401", () => {
       .send({
         id: 1,
         nombre: "Fran",
-        raza: "Humano",
+        raza: "Dragón",
         ubicacion: "Novigrado"
       })
       .expect(401);
@@ -83,6 +83,15 @@ describe("GET /cazadores?query, 200", () => {
       })
       .expect(201);
     await request(app)
+      .post("/cazadores")
+      .send({
+        id: 2,
+        nombre: "Fran",
+        raza: "Humano",
+        ubicacion: "Novigrado"
+      })
+      .expect(201);
+    await request(app)
       .get("/cazadores?nombre=Daniel")
       .expect(200);
   });
@@ -91,7 +100,7 @@ describe("GET /cazadores?query, 200", () => {
 describe("GET /cazadores/:id, 404", () => {
   test("No debería obtener, puesto que no hay cazadores.", async () => {
     await request(app)
-      .get("/cazadores/1")
+      .get("/cazadores/000000000000000000000000")
       .expect(404);
   });
 });
@@ -107,8 +116,10 @@ describe("GET /cazadores/:id, 200", () => {
         ubicacion: "Novigrado"
       })
       .expect(201);
+      const cazador = await Cazador.findOne({ nombre: "Daniel" });
+      const id = cazador._id;
     await request(app)
-      .get("/cazadores/1")
+      .get(`/cazadores/${id}`)
       .expect(200);
   });
 });
@@ -117,7 +128,7 @@ describe("GET /cazadores/:id, 200", () => {
 describe("DELETE /cazadores/:id, 404", () => {
   test("No debería eliminar, puesto que no hay cazadores.", async () => {
     await request(app)
-      .delete("/cazadores/1")
+      .delete("/cazadores/000000000000000000000000")
       .expect(404);
   });
 });
@@ -133,8 +144,216 @@ describe("DELETE /cazadores/:id, 200", () => {
         ubicacion: "Novigrado"
       })
       .expect(201);
+      const cazador = await Cazador.findOne({ nombre: "Daniel" });
+      const id = cazador._id;
     await request(app)
-      .delete("/cazadores/1")
+      .delete(`/cazadores/${id}`)
+      .expect(200);
+  });
+});
+
+describe("DELETE /cazadores?query, 404", () => {
+  test("No debería eliminar, puesto que no hay cazadores.", async () => {
+    await request(app)
+      .delete("/cazadores?nombre=Fran")
+      .expect(404);
+  });
+});
+
+describe("DELETE /cazadores?query, 403", () => {
+  test("No debería eliminar, puesto que hay varios cazadores con el mismo nombre.", async () => {
+    await request(app)
+      .post("/cazadores")
+      .send({
+        id: 1,
+        nombre: "Daniel",
+        raza: "Humano",
+        ubicacion: "Novigrado"
+      })
+      .expect(201);
+    await request(app)
+      .post("/cazadores")
+      .send({
+        id: 2,
+        nombre: "Daniel",
+        raza: "Humano",
+        ubicacion: "Novigrado"
+      })
+      .expect(201);
+    await request(app)
+      .delete("/cazadores?nombre=Daniel")
+      .expect(403);
+  });
+});
+
+describe("DELETE /cazadores?query, 200", () => {
+  test("Debería eliminar correctamente cazadores.", async () => {
+    await request(app)
+      .post("/cazadores")
+      .send({
+        id: 1,
+        nombre: "Daniel",
+        raza: "Humano",
+        ubicacion: "Novigrado"
+      })
+      .expect(201);
+    await request(app)
+      .delete("/cazadores?nombre=Daniel")
+      .expect(200);
+  });
+});
+
+//// TESTS DE PATCH ////
+describe("PATCH /cazadores/:id, 404", () => {
+  test("No debería actualizar, puesto que no hay cazadores.", async () => {
+    await request(app)
+      .patch("/cazadores/000000000000000000000000")
+      .send({
+        nombre: "Fran",
+        raza: "Humano",
+        ubicacion: "Skellige"
+      })
+      .expect(404);
+  });
+});
+
+describe("PATCH /cazadores/:id, 200", () => {
+  test("Debería actualizar correctamente cazadores.", async () => {
+    await request(app)
+      .post("/cazadores")
+      .send({
+        id: 1,
+        nombre: "Daniel",
+        raza: "Humano",
+        ubicacion: "Novigrado"
+      })
+      .expect(201);
+      const cazador = await Cazador.findOne({ nombre: "Daniel" });
+      const id = cazador._id;
+    await request(app)
+      .patch(`/cazadores/${id}`)
+      .send({
+        nombre: "Fran",
+        raza: "Humano",
+        ubicacion: "Skellige"
+      })
+      .expect(200);
+  });
+});
+
+describe("PATCH /cazadores/:id, 401", () => {
+  test("Debería prohibir la actualizacion del cazador.", async () => {
+    await request(app)
+      .post("/cazadores")
+      .send({
+        id: 1,
+        nombre: "Daniel",
+        raza: "Humano",
+        ubicacion: "Novigrado"
+      })
+      .expect(201);
+      const cazador = await Cazador.findOne({ nombre: "Daniel" });
+      const id = cazador._id;
+    await request(app)
+      .patch(`/cazadores/${id}`)
+      .send({
+        id: 3,
+        nombre: "Fran",
+        raza: "Humano",
+        ubicacion: "Skellige"
+      })
+      .expect(401);
+  });
+});
+
+describe("PATCH /cazadores?query, 404", () => {
+  test("No debería actualizar, puesto que no hay cazadores.", async () => {
+    await request(app)
+      .patch("/cazadores?nombre=Fran")
+      .send({
+        id: 1,
+        nombre: "Daniel",
+        raza: "Humano",
+        ubicacion: "Novigrado"
+      })
+      .expect(404);
+  });
+});
+
+describe("PATCH /cazadores?query, 403", () => {
+  test("No debería actualizar, puesto que hay varios cazadores con el mismo nombre.", async () => {
+    await request(app)
+      .post("/cazadores")
+      .send({
+        id: 1,
+        nombre: "Daniel",
+        raza: "Humano",
+        ubicacion: "Novigrado"
+      })
+      .expect(201);
+    await request(app)
+      .post("/cazadores")
+      .send({
+        id: 2,
+        nombre: "Daniel",
+        raza: "Humano",
+        ubicacion: "Novigrado"
+      })
+      .expect(201);
+    await request(app)
+      .patch("/cazadores?nombre=Daniel")
+      .send({
+        nombre: "Fran",
+        raza: "Humano",
+        ubicacion: "Novigrado"
+      })
+      .expect(403);
+  });
+});
+
+describe("PATCH /cazadores?query, 200", () => {
+  test("Debería actualizar correctamente cazadores.", async () => {
+    await request(app)
+      .post("/cazadores")
+      .send({
+        id: 1,
+        nombre: "Daniel",
+        raza: "Humano",
+        ubicacion: "Novigrado"
+      })
+      .expect(201);
+    await request(app)
+      .patch("/cazadores?nombre=Daniel")
+      .send({
+        nombre: "Fran",
+        raza: "Humano",
+        ubicacion: "Novigrado"
+      })
+      .expect(200);
+  });
+});
+
+describe("PATCH /cazadores?query, 200", () => {
+  test("Debería prohibir la actualización de cazadores.", async () => {
+    await request(app)
+      .post("/cazadores")
+      .send({
+        id: 1,
+        nombre: "Daniel",
+        raza: "Humano",
+        ubicacion: "Novigrado"
+      })
+      .expect(201);
+    const cazador = await Cazador.findOne({ nombre: "Daniel" });
+    const id = cazador._id;
+    await request(app)
+      .patch(`/cazadores/${id}`)
+      .send({
+        id: 3,
+        nombre: "Fran",
+        raza: "Humano",
+        ubicacion: "Novigrado"
+      })
       .expect(200);
   });
 });
